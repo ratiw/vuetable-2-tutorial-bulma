1e7fd1fb71d15c200575e3374c439f94b16d4656
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-01-18 21:08:42 +0700

    lesson-1

---
+++ src/components/MyVuetable.vue
@@ -0,0 +1,24 @@
+<template>
+  <vuetable ref="vuetable"
+    api-url="http://vuetable.ratiw.net/api/users"
+    :fields="['name', 'email', 'birthdate']"
+    :css="css"
+  ></vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+
+export default {
+  components: {
+    Vuetable
+  },
+  data () {
+    return {
+      css: {
+        tableClass: 'table is-bordered is-striped'
+      }
+    }
+  }
+}
+</script>
